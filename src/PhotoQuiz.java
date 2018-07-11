b8205ac83b2cfac4e884d85dfd36e58aa1c697e2
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-07-11 16:59:25 -0700

    MyFirstSwingGUI, PhotoQuizic

--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -22,25 +22,29 @@ public class PhotoQuiz {
                 quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // This will make sure the program exits when you close the window
 
 		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
-
+        String URL = ("https://hatrabbits.com/wp-content/uploads/2016/12/rare-combinaties.jpg");
 		// 2. create a variable of type "Component" that will hold your image
-
-		// 3. use the "createImage()" method below to initialize your Component
-
+        Component Holder;
+        // 3.use the "createImage()" method below to initialize your Component
+        Holder = createImage(URL);
 		// 4. add the image to the quiz window
-
+        quizWindow.add(Holder);
 		// 5. call the pack() method on the quiz window
-
+        quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+        String answer = JOptionPane.showInputDialog(null, "What Animal Is It?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+        if(answer.equalsIgnoreCase("lion")) {
+        	JOptionPane.showMessageDialog(null, "CORRECT");
+        }
 		// 8. print "INCORRECT" if the answer is wrong
-
+        else {
+        	JOptionPane.showMessageDialog(null, "INCORRECT");
+        }
 		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
-
+        quizWindow.remove(Holder);
 		// 10. find another image and create it (might take more than one line of code)
-
+        
 		// 11. add the second image to the quiz window
 
 		// 12. pack the quiz window
